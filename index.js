bbc30c24fa26de9229534749d36f80f05a3f898a
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2022-12-08 10:57:54 -0600

    Create basic endpoint

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello!")
+// Server with endpoints using route parameters
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Set up endpoint at the root path
+app.get('/', (req, res) => {
+    res.send("Successful GET Request heard!")
+})
+
+app.listen(3000, () => {
+    console.log(`Server listening at http://localhost:${port}`)
+})
