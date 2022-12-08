b96f968e96e7672ea62dc7e6765a02ff011150ea
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2022-12-08 11:04:08 -0600

    Create endpoint the has route parameter

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ app.get('/', (req, res) => {
     res.send("Successful GET Request heard!")
 })
 
+// Endpoint 2
+app.get('/users/:id', (req, res) => {
+    // extract the data from the request
+    const userId = req.params.id;
+    // ...send out a response back, based on the request
+    res.send(`You are a wonderful person, Person #${userId}`);
+})
+
 app.listen(3000, () => {
     console.log(`Server listening at http://localhost:${port}`)
 })
