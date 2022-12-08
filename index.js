3bf9756fefc98844a56d4bfa7692eaeebf73c2da
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2022-12-08 11:13:56 -0600

    Create endpoint with multiple parameters

--- index.js
+++ index.js
@@ -9,12 +9,22 @@ app.get('/', (req, res) => {
     res.send("Successful GET Request heard!")
 })
 
+// another endpoint with a path
+app.get('/users', (req, res) => {
+    res.send("Successful GET Request heard, from the 'users' path")
+})
+
 // Endpoint 2
 app.get('/users/:id', (req, res) => {
     // extract the data from the request
     const userId = req.params.id;
     // ...send out a response back, based on the request
-    res.send(`You are a wonderful person, Person #${userId}`);
+    res.send(`You are a FANTASTIC person, Person #${userId}`);
+})
+
+// Endpoint 3
+app.get('/users/:name/:numOfCookies', (req, res) => {
+    res.send(`You are great, ${req.params.name}, and you can have ${req.params.numOfCookies} cookies!`);
 })
 
 app.listen(3000, () => {
